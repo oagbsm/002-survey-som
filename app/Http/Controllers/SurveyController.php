27a6b3202d62f5bfc85adb1d539d5ade660317cd
liiban <a.yassin@Ayans-MacBook-Air.local>
2024-09-27 00:59:51 +0300

    like 15 percent of the way

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -36,37 +36,67 @@ class SurveyController extends Controller
         // Pass the surveys to the view
         return view('user.dashboard', compact('surveys'));
     }
+
     public function submitAnswers(Request $request, $id)
     {
-        // Validate the incoming request
-        $request->validate([
-            'responses.' . $id . '.*' => 'required|string', // Ensure all responses are filled out
-        ]);
+        $surveyId = $request->input('survey_id');
+        $answers = $request->input('answers');
+    
+        // Create an array to hold the answers with their indices
+        $formattedAnswers = [];
+    
+        foreach ($answers as $index => $answer) {
+            // If the answer is an array (e.g., checkbox answers), convert it to a string
+            if (is_array($answer)) {
+                $answer = implode(',', $answer);
+            }
+    
+            // Add the question index and answer to the formatted array
+            $formattedAnswers[$index] = $answer;
+        }
+    
+        // Update the survey record with the formatted answers
+        $survey = Survey::find($surveyId);
+        $survey->answers = json_encode($formattedAnswers); // Store the answers as JSON
+        $survey->save();
+    
+        // For debugging, you can use dd to see the formatted answers
+        dd($formattedAnswers);
+    
+        return redirect()->route('surveys.index')->with('success', 'Responses submitted successfully!');
+    }
+
 
         // Get the responses
-        $responses = $request->input('responses')[$id];
+        // $responses = $request->input('responses')[$id];
 
-        // Instead of saving, echo the responses
-        $responseString = "Responses for Survey ID {$id}:<br>";
+        // // Instead of saving, echo the responses
+        // $responseString = "Responses for Survey ID {$id}:<br>";
         
-        foreach ($responses as $questionIndex => $response) {
-            $responseString .= "Question {$questionIndex}: {$response}<br>";
-        }
+        // foreach ($responses as $questionIndex => $response) {
+        //     $responseString .= "Question {$questionIndex}: {$response}<br>";
+        // }
 
-        return response()->json([
-            'success' => true,
-            'message' => $responseString,
-        ]);
-    }
+        // return response()->json([
+        //     'success' => true,
+        //     'message' => $responseString,
+        // ]);
+    
 
     public function show($id)
     {
-        // Retrieve the survey by ID, or fail if not found
-        $survey = Survey::findOrFail($id);
+        // Retrieve the survey by ID or fail if not found
+        $survey = Survey::findOrFail($id); 
+        
+        // Assuming you store questions and options as JSON
+        $questions = json_decode($survey->questions); // Decode questions from JSON
+        $allOptions = explode(',', $survey->options); // Split the options into an array
 
         // Pass the survey data to the view
         return view('user.survey_detail', [
             'survey' => $survey,
+            'questions' => $questions, // Pass questions to the view
+            'allOptions' => $allOptions, // Pass all options to the view
         ]);
     }
 
